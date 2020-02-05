b6d2eca73f5dc250a6a18e9e0a18419dfeff811e
Avery <avecolth@gmail.com>
2020-02-05 11:48:39 -0800

    Camera Follow
    added a following camera and a delta time

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 Offset = new Vector3(0, 7, -10);
+    // the private is to set up where the camera is offset from the gameobject(the vehicle)
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // the transform is the object that it's attached to (the camera) ((called scoping))
+        transform.position = player.transform.position + Offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+    
+{
+    // to make the speed adjustable, starts at 0
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, speed);
+    }
+}
